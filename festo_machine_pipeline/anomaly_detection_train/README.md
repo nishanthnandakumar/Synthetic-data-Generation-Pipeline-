5454dabfed66a6a2c48ff3c298778fcedafbf8e7
Nishanth Nandakumar <nishanth.n2494@gmail.com>
2025-09-25 11:50:38 +0200

    Add files via upload

---
+++ festo_machine_pipeline/anomaly_detection_train/README.md
@@ -0,0 +1,111 @@
+# anomaly_detection
+
+This repository is used for anomaly detection of parts using the [anomalib](https://github.com/enrico310786/Image_Anomaly_Detection/tree/main). This is based on the [online](https://medium.com/@enrico.randellini/anomalib-a-library-for-image-anomaly-detection-and-localization-fb363639104f) proceeding as discussed. This would result in providing the anomaly detection of the required dataset.
+
+## Getting started
+
+Clone this repository, and make sure the location of the dataset is correct in the docker-compose file.
+
+Run the following commands in the terminal:
+
+```
+xhost local:root 
+```
+Then cd into the docker repository and Run
+
+```
+docker build -t anomaly_detection_festo -f Dockerfile .
+```
+
+
+Make sure the locations are right in the docker-compose file for all the repositories and the dataset.
+
+
+and run 
+
+```
+docker-compose up
+```
+
+Now, in another terminal run the following command,
+
+```
+docker exec -it anomaly_detection_festo /bin/bash
+```
+
+This would access the running container. Once inside cd into src and run the following command to train the model.
+
+1. The main repo is mounted onto the container in the docker volumes.
+
+2. The first step is to mount the custom dataset. This is acheived by adding the dataset to the src repository. It has the following structure,
+
+    dataset
+    |---festo
+        |---orange
+        |   |---fixed
+        |   |---abnormal
+        |---black
+        |   |---fixed
+        |   |---abnormal
+        |---silver
+        |   |---fixed
+        |   |---abnormal
+
+
+    Add the anomaly images in the abnormal repositories and good images in the fixed repositories.
+
+3. Edit the ./src/Image_Anomaly_Detection-main/data/inspect_FolderDataModule.py to create the data tensors. Edit lines 12, 26-38, 108-118.
+
+4. Now on inside of the container, cd to src/Image_Anomaly_Detection-main/data and run the following command,
+
+```
+python3 inspect_FolderDataModule.py
+```
+
+5. After this cd to Image_Anomaly_Detection-main repository and run the following command,
+
+```
+python3 ./train_anomalib/train_reversedistillation_anomalib.py --dataset_root /root/src/dataset/festo/orange --name_normal_dir fixed --name_wandb_experiment revdist_orange_v1 --name orange --max_epochs 100 --patience 10
+```
+
+6. The trained model can be tested using the following command,
+
+```
+python3 ./infer_anomalib/test_model.py --path_torch_model /root/src/Image_Anomaly_Detection-main/results/ReverseDistillation/orange/v0/weights/torch/model.pt --path_dataset /root/src/dataset/festo/orange --name orange --dir_result /root/src/Image_Anomaly_Detection-main/results/ReverseDistillation/orange/v0
+```
+
+and infer using the following command,
+
+```
+python3 ./infer_anomalib/infer_oneshot.py --path_torch_model /root/src/Image_Anomaly_Detection-main/results/ReverseDistillation/orange/v0/weights/torch/model.pt  --path_image /root/src/dataset/festo/orange/abnormal/0068.png --path_result /root/src/Image_Anomaly_Detection-main/results/ReverseDistillation/orange/v0/example_inference.png 
+```
+
+## External Dependencies
+
+The following are required to run the project successfully,
+
+1. docker
+2. docker compose
+3. nvidia docker
+4. pytorch gpu
+5. CuDA and CuDNN
+
+## Build Dependencies
+
+None
+
+## Run Dependencies
+
+None
+
+## Authors
+
+Nishanth Nandakumar @nandakun
+
+## License
+
+For open source projects, say how it is licensed.
+
+## Project Status
+
+Ongoing
---
+++ festo_machine_pipeline/anomaly_detection_train/src/Image_Anomaly_Detection-main/data/inspect_FolderDataModule.py
@@ -0,0 +1,118 @@
+import numpy as np
+from anomalib.data.utils import ValSplitMode
+from torchvision.transforms.v2.functional import to_pil_image
+from anomalib.data.image.folder import Folder
+from anomalib import TaskType
+import os
+import torch
+
+from utils import show_image_list
+
+# set the dataset root for a particular category
+dataset_root = "/root/src/dataset/festo1/"
+
+
+'''
+val_split_mode (ValSplitMode): Setting that determines how the validation subset is obtained.
+    Defaults to ``ValSplitMode.FROM_TEST``.
+val_split_ratio (float): Fraction of train or test images that will be reserved for validation.
+    Defaults to ``0.5``.
+    
+By default the validation set is created from the test set -> this is controlled by val_split_mode=ValSplitMode.FROM_TEST
+The fraction of test data used to build the validation data is set by the parameter val_split_ratio uqual to 0.5 by dedfault
+'''
+
+# Create the datamodule
+datamodule = Folder(
+    name="festo1",
+    root=dataset_root,
+    normal_dir="fixed",
+    abnormal_dir="abnormal",
+    task=TaskType.CLASSIFICATION,
+    seed=42,
+    normal_split_ratio=0.2, # default value
+    val_split_mode=ValSplitMode.FROM_TEST, # default value
+    val_split_ratio=0.5, # default value
+    train_batch_size=8, # default value
+    eval_batch_size=8, # default value
+    image_size=(460,120)
+)
+
+# Setup the datamodule
+datamodule.setup()
+
+'''
+https://anomalib.readthedocs.io/en/v1.0.1/markdown/guides/how_to/data/custom_data.html
+
+The Folder datamodule will create training, validation, test and prediction datasets and dataloaders for us. 
+We can access the datasets and dataloaders by following the same approach as in the segmentation task.
+
+When we check the samples from the dataloaders, we will see that the mask key is not present in the samples. 
+This is because we do not need the masks for the classification task.
+
+'''
+
+# Train images
+i, data_train = next(enumerate(datamodule.train_dataloader()))
+print(data_train.keys(), data_train["image"].shape) # it takes a batch of images
+# for each key extract the first image
+print("data_train['image_path'][0]: {} - data_train['image'][0].shape: {} - data_train['label'][0]: {} - torch.max(data_train['image][0]): {} - torch.min(data_train['image][0]): {}".format(data_train['image_path'][0], data_train['image'][0].shape, data_train['label'][0], torch.max(data_train['image'][0]), torch.min(data_train['image'][0])))
+img_train = to_pil_image(data_train["image"][0].clone())
+
+# val images
+i, data_val = next(enumerate(datamodule.val_dataloader()))
+# for each key extract the first image
+print("data_val['image_path'][0]: {} - data_val['image'][0].shape: {} - data_val['label'][0]: {}".format(data_val['image_path'][0], data_val['image'][0].shape, data_val['label'][0]))
+img_val = to_pil_image(data_val["image"][0].clone())
+
+
+# test images
+i, data_test = next(enumerate(datamodule.test_dataloader()))
+# for each key extract the first image
+print("data_test['image_path'][0]: {} - data_test['image'][0].shape: {} - data_test['label'][0]: {}".format(data_test['image_path'][0], data_test['image'][0].shape, data_test['label'][0]))
+img_test = to_pil_image(data_test["image"][0].clone())
+
+
+# from the datamodule extract the train, val and test Pandas dataset and collect all the info in a csv
+train_dataset = datamodule.train_data.samples
+print("train_dataset.head()")
+print(train_dataset[['label','label_index']].head())
+test_dataset = datamodule.test_data.samples
+val_dataset = datamodule.val_data.samples
+
+# check the data distribution for each category in each data split
+print("TRAIN DATASET FEATURES")
+print(train_dataset.info())
+print("")
+print("IMAGE DISTRIBUTION BY CLASS")
+print("")
+desc_grouped = train_dataset[['label']].value_counts()
+print(desc_grouped)
+print("----------------------------------------------------------")
+print("TEST DATASET FEATURES")
+print(test_dataset.info())
+print("")
+print("IMAGE DISTRIBUTION BY CLASS")
+print("")
+desc_grouped = test_dataset[['label']].value_counts()
+print(desc_grouped)
+print("----------------------------------------------------------")
+print("VAL DATASET FEATURES")
+print(val_dataset.info())
+print("")
+print("IMAGE DISTRIBUTION BY CLASS")
+print("")
+desc_grouped = val_dataset[['label']].value_counts()
+print(desc_grouped)
+
+datamodule.train_data.samples.to_csv(os.path.join("/root/src/Image_Anomaly_Detection-main/data", "datamodule_train.csv"), index=False)
+datamodule.test_data.samples.to_csv(os.path.join("/root/src/Image_Anomaly_Detection-main/data", "datamodule_test.csv"), index=False)
+datamodule.val_data.samples.to_csv(os.path.join("/root/src/Image_Anomaly_Detection-main/data", "datamodule_val.csv"), index=False)
+
+# show_image_list(list_images=[np.array(img_train), np.array(img_val), np.array(img_test)],
+#                 list_titles=['train', 'validation', 'test'],
+#                 num_cols=3,
+#                 figsize=(20, 10),
+#                 grid=False,
+#                 title_fontsize=20,
+#                 path_image="/root/src/Image_Anomaly_Detection-main/resources/examples_images.png")
